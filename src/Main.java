2aa0ed7e68773b6a9385d221c69088ca2ac854b3
Anas Ezzughayyar <anas.ezzughayyar@gmail.com>
2026-04-02 15:28:56 +0300

    solve leetcode problems using JAVA

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     System.out.println("Hello World!");
 
+    ProductExceptSelf productExceptSelf = new ProductExceptSelf();
+    int[] nums = {1, 2, 3, 4};
+    int[] result = productExceptSelf.productExceptSelf(nums);
+    System.out.println(Arrays.toString(result));
 }
